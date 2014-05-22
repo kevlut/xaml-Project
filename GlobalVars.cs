7163ad4bbbe2a7ba94e4327911099465b964d162
user2509848 <abrahamhorsch@gmail.com>
2014-05-22 11:58:29 -0500

    Update GlobalVars.cs

--- GlobalVars.cs
+++ GlobalVars.cs
@@ -2,13 +2,8 @@ namespace LearnOneNote
 {
     public static class GlobalVars
     {
-        private static string[] _home = { "OneNote", "   The Main Menu" };
-
-        private static string[] _mainMenu = { "      Draw" };
+        private static string[] _home = { "OneNote", "   The Main Menu", "   The Text Menu" };
 
         public static string[] Home() { return _home; }
-        public static string[] MainMenu() { return _mainMenu; }
-
-        public static string[] Text = Home();
     }
 }
